329d46c48e87e056dde12d05383abf0e2c84f090
JoE11-y <Josephedoh77@gmail.com>
2026-03-24 04:12:14 +0100

    feat: add health endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { CoreModule } from './core/core.module'
+import { HealthModule } from './modules/health/health.module'
 import { VerifyModule } from './modules/verify/verify.module'
 
 @Module({
-  imports: [CoreModule, VerifyModule],
+  imports: [CoreModule, HealthModule, VerifyModule],
 })
 export class AppModule {}
